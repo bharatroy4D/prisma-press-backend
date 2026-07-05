4a710182ff21b9b363b812ed2a8bba92d854b35a
bharat roy <bharatroy1005@gmail.com>
2026-07-05 09:30:42 +0600

    lib folder into prisma.ts file create

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import app from "./app";
-const PORT = 5000;
+import { prisma } from "./lib/prisma";
+import "dotenv/config";
+
+const PORT = process.env.PORT;
 
 const main = async () => {
     try {
+        // await prisma.$connect();
+        // console.log("connect to the databse successfully");
         app.listen(PORT, () => {
             console.log(`server is running port ${PORT}`);
         })
     } catch (error) {
         console.error(error);
+        // await prisma.$disconnect();
+        process.exit(1)
     }
 }
 main();
