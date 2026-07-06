68fc9f688b197b1b016f39439fb72231c4d4c128
bharat roy <bharatroy1005@gmail.com>
2026-07-06 08:26:22 +0600

    User controller is created

---
+++ src/modules/users/user.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs";
+import config from "../../config";
+import HttpStatus from "http-status";
+import { userService } from "./user.service";
+
+
+const RegisterUser = async (req: Request, res: Response) => {
+    const payload = req.body;
+    const user = await userService.RigisterUserIntoDB(payload)
+
+    res.status(HttpStatus.CREATED).json({
+        success: true,
+        successStatus: HttpStatus.CREATED,
+        message: "User Register Successfully",
+        data: {
+            user
+        }
+    })
+}
+export const userController = {
+    RegisterUser
+}
