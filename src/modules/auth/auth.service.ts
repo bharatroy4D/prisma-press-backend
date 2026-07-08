3a3363829955b7e1624d6b2e61b70c58ae01b7bd
bharat roy <bharatroy1005@gmail.com>
2026-07-08 10:06:06 +0600

    User login for db query run

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,23 @@
-const userLoginIntoDB = async () => {
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+import { ILoginUser } from "./auth.interface"
 
+const userLoginIntoDB = async (payload: ILoginUser) => {
+    const { email, password } = payload;
+
+    const user = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if (!user) {
+        throw new Error("User not found")
+    };
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+        throw new Error("Password is Invalid")
+    };
+    return user;
 }
 
 export const authService = {
